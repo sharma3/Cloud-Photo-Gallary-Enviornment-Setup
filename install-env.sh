423edd1a1ef95dab5358f45da946285e7fdfd553
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-04 04:05:38 -0500

    added mv command for Page2

--- install-env.sh
+++ install-env.sh
@@ -8,6 +8,7 @@ git clone https://github.com/sharma3/ITMO-544-A20344475-Application-Setup.git
 
 mv ./ITMO-544-A20344475-Application-Setup/images /var/www/html/images
 mv ./ITMO-544-A20344475-Application-Setup/index.html /var/www/html
+mv ./ITMO-544-A20344475-Application-Setup/Page2.html /var/www/html
 
 echo "Hello" > /tmp/hello.txt
 
