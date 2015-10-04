026f4cb063a73d33587481ea88524470e92edc76
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-04 03:43:03 -0500

    added webserver configuration ec2 user

--- install-env.sh
+++ install-env.sh
@@ -1,8 +1,15 @@
-#!bin/bash
+#!/bin/bash
 
 
 sudo apt-get update -y
-sudo apt-get install apache2 -y git
+sudo apt-get install -y apache2 git
+
+groupadd www
+usermod -a -G www ec2-user
+chown -R root:www /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} +
+find /var/www -type f -exec chmod 0664 {} +
 
 git clone https://github.com/sharma3/ITMO-544-A20344475-Application-Setup.git
 
