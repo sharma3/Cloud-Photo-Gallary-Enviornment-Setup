db94ae33c214299d86c4d2a7ee7e9d78b4fe04a6
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-04 02:56:20 -0500

    clone new repository and added index.html in install-env.sh

--- install-env.sh
+++ install-env.sh
@@ -2,7 +2,17 @@
 
 
 sudo apt-get update -y
-sudo apt-get install apache2 -y
+sudo apt-get install apache2 -y git
+
+git clone https://github.com/sharma3/ITMO-544-A20344475-Application-Setup.git
+
+mv ./ITMO-544-A20344475-Application-Setup/images /var/www/html/images
+mv ./ITMO-544-A20344475-Application-Setup/index.html /var/www/html
+
+echo "Hello" > /tmp/hello.txt
+
+
+
 
 
 
