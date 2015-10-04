f26629b49d2335a017de14fdd5369f94057177aa
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-04 03:59:17 -0500

    removed ec2 user credentials

--- install-env.sh
+++ install-env.sh
@@ -4,13 +4,6 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-groupadd www
-usermod -a -G www ec2-user
-chown -R root:www /var/www
-chmod 2775 /var/www
-find /var/www -type d -exec chmod 2775 {} +
-find /var/www -type f -exec chmod 0664 {} +
-
 git clone https://github.com/sharma3/ITMO-544-A20344475-Application-Setup.git
 
 mv ./ITMO-544-A20344475-Application-Setup/images /var/www/html/images
