eba20767abd1d9b377378c8ba9e4531036298771
Jay Sharma <jsharma3@hawk.iit.edu>
2015-10-28 20:59:04 -0500

    Added PHP5 SDK and curl for congiguration

--- install-env.sh
+++ install-env.sh
@@ -2,7 +2,7 @@
 
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php5 mysql-client
+sudo apt-get install -y apache2 git php5 php5-curl mysql-client curl php5-mysql
 
 git clone https://github.com/sharma3/ITMO-544-A20344475-Application-Setup.git
 
@@ -11,7 +11,15 @@ mv ./ITMO-544-A20344475-Application-Setup/index.html /var/www/html
 mv ./ITMO-544-A20344475-Application-Setup/Page2.html /var/www/html
 mv ./ITMO-544-A20344475-Application-Setup/*.php /var/www/html
 
-echo "Hello" > /tmp/hello.txt
+curl -sS https://getcomposer.org/installer | sudo php &> /tmp/getcomposer.txt
+
+sudo php composer.phar require aws/aws-sdk-php &> /tmp/runcomposer.txt
+
+sudo mv vendor /var/www/html &> /tmp/movevendor.txt
+
+sudo php /var/www/html/setup.php &> /tmp/database-setup.txt
+
+echo "Hello!" > /tmp/hello.txt
 
 
 
